c59b0eb1ba6a047b6c483d215df45f9fca2b23bc
Cristina <criss.ms7@gmail.com>
2020-07-21 11:45:13 -0500

    dir top

--- e1039-analysis/SimHits/macro/Fun4SimHit.C
+++ e1039-analysis/SimHits/macro/Fun4SimHit.C
@@ -130,7 +130,7 @@ int Fun4SimHit(
   gSystem->Load("libsim_eval.so");
   SimEval *sim_eval = new SimEval();
   sim_eval->set_hit_container_choice("Vector");
-  stringstream ssout; ssout << "$DIR_PWD/macro/simeval_electrons_emcal/sim_eval_" << ifile << ".root";
+  stringstream ssout; ssout << "$DIR_TOP/macro/simeval_electrons_emcal/sim_eval_" << ifile << ".root";
   //stringstream ssout; ssout << "sim_eval_" << ifile << ".root";
   sim_eval->set_out_name(ssout.str().c_str());
   //sim_eval->Verbosity(10);
