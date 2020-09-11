16c34cdcf4044be498b524b304851dd54edcd0ef
Cristina <criss.ms7@gmail.com>
2020-09-11 09:43:30 -0500

    add setup and instructions

--- e1039-analysis/DataHits/macro/setup_mye1039.sh
+++ e1039-analysis/DataHits/macro/setup_mye1039.sh
@@ -1,4 +1,4 @@
 export DIR_TOP=$(dirname $(readlink -f $BASH_SOURCE))
 echo $DIR_TOP
-source /e906/app/software/osg/software/e1039/this-e1039.sh
+source /seaquest/users/cmantill/mye1039/core-inst/this-e1039.sh
 export LD_LIBRARY_PATH=$DIR_TOP/install/lib/:$LD_LIBRARY_PATH
