4fb94f501d9582c3fb47c4f09f618ad54d7c3a43
Cristina <criss.ms7@gmail.com>
2021-02-23 09:20:51 -0600

    remove old

--- e1039-analysis/SimHits/AnaModule/LinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-
-#pragma link off all class;
-#pragma link off all function;
-#pragma link off all global;
-
-#pragma link C++ class AnaModule-!;
-
-#endif
--- e1039-analysis/SimHits/oldSimEval/workana/CMakeFiles/install_pcm.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install_pcm"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install_pcm.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
