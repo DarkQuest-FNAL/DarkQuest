e4a8ca93140ad19573b82f2013c1cc0d9ec1f605
Yongbin Feng <ybfeng94@gmail.com>
2024-05-27 01:52:45 -0500

    update directory

--- e1039-analysis/SimHits/setup_mye1039.sh
+++ e1039-analysis/SimHits/setup_mye1039.sh
@@ -1,4 +1,4 @@
-export MY_E1039=/seaquest/users/yfeng/cleanTest/core-inst/this-e1039.sh
+export MY_E1039=/seaquest/users/yfeng/DarkQuest-FNAL/core-inst/this-e1039.sh
 source $MY_E1039
 
 export HepMC_DIR=/pnfs/e1039/persistent/users/yfeng/displaced_Aprime_Muons/
