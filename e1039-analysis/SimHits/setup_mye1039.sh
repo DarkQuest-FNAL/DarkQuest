aab45f4a0848672eee014aa10527137720031bb1
Yongbin Feng <ybfeng94@gmail.com>
2024-05-26 23:27:05 -0500

    fix minor config; update instructions; have things running on alma9

--- e1039-analysis/SimHits/setup_mye1039.sh
+++ e1039-analysis/SimHits/setup_mye1039.sh
@@ -1,8 +1,7 @@
-#export MY_E1039=/home/dsperka/DarkQuest-FNAL/core-inst/this-e1039.sh
-export MY_E1039=/home/dsperka/wpmccormack/core-inst/this-e1039.sh
-export DIR_TOP=$(dirname $(readlink -f $BASH_SOURCE))
-echo $MY_E1039
-echo $DIR_TOP
+export MY_E1039=/seaquest/users/yfeng/cleanTest/core-inst/this-e1039.sh
 source $MY_E1039
+
+export HepMC_DIR=/pnfs/e1039/persistent/users/yfeng/displaced_Aprime_Muons/
+export DIR_TOP=$(dirname $(readlink -f $BASH_SOURCE))
+echo $DIR_TOP/install/lib
 export LD_LIBRARY_PATH=$DIR_TOP/install/lib/:$LD_LIBRARY_PATH
-export DIR_CMANTILL=/cms/data/seaquest/users/cmantill/DarkQuest/e1039-analysis/SimHits
